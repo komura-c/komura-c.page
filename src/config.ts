97156e581fd90286d8e0d5a539918d8ebd55ab61
komura <affirman03139@outlook.jp>
2023-01-19 04:02:50 +0900

    feat: add blog route page, and fetch blog content

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "komura-c.github.io";
-export const SITE_DESCRIPTION = "komura-cのポートフォリオ・ブログサイトです。";
+export const SITE_DESCRIPTION = "komura-cのポートフォリオ、ブログサイトです。";
---
+++ src/types/Article.ts
@@ -0,0 +1,6 @@
+export interface Article {
+  title: string;
+  url: string;
+  pubDate: string;
+  isMySite: boolean;
+}
