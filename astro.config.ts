96dd3588174d31201d93ca9dd84ebdb7644d810a
komura-c <37304826+komura-c@users.noreply.github.com>
2024-03-16 23:19:12 +0900

    fix: url, remove file

--- astro.config.ts
+++ astro.config.ts
@@ -3,4 +3,14 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://komura-c.page",
+  // redirects: {
+  //   "/blog": {
+  //     status: 301,
+  //     destination: "https://blog.komura-c.page",
+  //   },
+  //   "/blog/[...slug]": {
+  //     status: 301,
+  //     destination: "https://blog.komura-c.page/posts/[...slug]",
+  //   },
+  // },
 });
